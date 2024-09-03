478295af415fb489ba547840b74842a2b5fd560e
harishraajaa <rvharishraajaa@gmail.com>
2024-09-03 10:23:28 +0530

    Updated Cors

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import appRoutes from './src/Router/index.router.js'
 import 'dotenv/config'
+import cors from 'cors'
 
 const PORT = process.env.PORT
 const app=express()
-
+app.use(cors())
 app.use(express.json())
 //app.use(express.urlencoded())
 
